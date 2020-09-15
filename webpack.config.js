11f284f26eec4fd799bcbc83c3e114f2a1d017c3
Oleg Ledvanov <olegledvanov@gmail.com>
2020-09-16 01:27:24 +0300

    install webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
-const path = require('path')
+const path = require('path') 
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
+    //context - за какой папкой смотреть
+    context: path.resolve(__dirname, 'src'), // возращает абс.путь до текущий дирректории + /src
+    //mode - режим вебпака
     mode: 'development',
+    //входная точка для приложения
     entry: './index.js',
+    //имя и путь для output bundle (сжатый код):
     output: {
-        filename: 'bundle.js',
+        //[hash] - вставляет хэш в имя файла, чтобы избежать проблемы с кэшированием
+        filename: 'bundle.[hash].js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        
+    ]
 }
